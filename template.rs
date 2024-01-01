24307538c34e0962f01e83220b264c9112e66990
Gordin508 <5062634+Gordin508@users.noreply.github.com>
2024-01-01 12:28:15 +0100

    Add simple benchmark to template

--- template.rs
+++ template.rs
@@ -14,6 +14,7 @@ fn part2(lines: &Vec<&str>) -> Option<i64> {
 }
 
 fn main() {
+    use std::time::Instant;
     let args: Vec<String> =  env::args().collect();
     let infile = args.get(1).unwrap_or_else(|| {
         println!("Usage: {} <puzzle input>", args[0]);
@@ -29,9 +30,10 @@ fn main() {
     // later parts may follow, so we loop over the part functions
     let parts = [part1, part2];
     for (index, part) in parts.iter().enumerate() {
+        let partstart = Instant::now();
         let result = part(&lines);
         match result {
-            Some(result) => println!("Part {}: {}", index+1, result),
+            Some(result) => println!("Part {}: {}\t({:.3?} s)", index+1, result, partstart.elapsed().as_secs_f64()),
             None => println!("Part {}: No result", index+1),
         }
     }

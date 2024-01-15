27b5b3a4af2e56aba9d5ff8826576d74940a5cca
Gordin508 <5062634+Gordin508@users.noreply.github.com>
2024-01-15 17:54:28 +0100

    Speed up part1

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -13,32 +13,60 @@ fn quantum_entanglement(presents: &[usize]) -> usize {
 
 #[derive(Debug, Clone)]
 struct DFSFrame {
-    lastassigned: usize,
-    assigned_to: usize,
+    pos: usize,
+    lastassigned: bool,
     entanglement: usize,
     front_presents: usize,
     forward: bool
 }
 
 impl DFSFrame {
-    fn new(lastassigned: usize, assigned_to: usize, entanglement: usize, front_presents: usize, forward: bool) -> DFSFrame {
-        DFSFrame {lastassigned, assigned_to, entanglement, front_presents, forward }
+    fn new(pos: usize, lastassigned: bool, entanglement: usize, front_presents: usize, forward: bool) -> DFSFrame {
+        DFSFrame{pos, lastassigned, entanglement, front_presents, forward }
     }
+}
+
+fn has_valid_combination(presents: &[usize], assigned: &mut [bool], weights: &[usize], target_weight: usize) -> bool {
+    assert!(assigned.len() == presents.len());
+    if weights.len() == 0 {
+        return true;
+    } else if weights.len() == 1 && weights[0] == target_weight {
+        return true;
+    }
+    assert!(weights[0] < target_weight);
+    let missing = target_weight - weights[0];
+    let unassigned: Vec<usize> = (0..presents.len()).filter(|n| !assigned[*n] && presents[*n] <= missing).collect();
+    for n in unassigned {
+        assigned[n] = true;
+        let present = presents[n];
+        let mut nweights = if present == missing { weights[1..].to_vec() } else { weights.to_vec() };
+        if present != missing {
+            nweights[0] += present;
+        }
+        assert!(nweights.len() == 0 || nweights[0] < target_weight);
+        if has_valid_combination(presents, assigned, &nweights, target_weight) {
+            return true;
+        }
+        assigned[n] = false;
+    }
+
+    false
 }
 
 fn balance(presents: &[usize], num_groups: usize) -> usize {
     let total = presents.iter().sum::<usize>();
     assert!(total % num_groups == 0);
-    let third = total / num_groups;
+    let partition_weight = total / num_groups;
 
-    let mut weights = vec![0usize; num_groups];
-    let mut assignments = vec![0; presents.len()];
+    let mut weight = 0usize;
+    let mut assigned = vec![false; presents.len()];
     let mut least_qe = usize::MAX;
     let mut least_presents = usize::MAX;
     let mut qe_upper_bound = 1usize;
     let mut lp_upper_bound = 0usize;
     let mut stack: Vec<DFSFrame> = Vec::new();
-    stack.push(DFSFrame::new(0, 0, 1, 0, true));
+    stack.push(DFSFrame::new(0, false, 1, 0, true));
+    stack.push(DFSFrame::new(0, true, 1, 0, true));
 
     while let Some(frame) = stack.pop() {
         if frame.forward {
@@ -47,45 +75,50 @@ fn balance(presents: &[usize], num_groups: usize) -> usize {
                 continue;
             }
 
-            if frame.assigned_to > 0 {
-                weights[frame.assigned_to - 1] += presents[frame.lastassigned];
-                assignments[frame.lastassigned] = frame.assigned_to;
+            if frame.lastassigned {
+                weight += presents[frame.pos];
+                assigned[frame.pos] = true;
             }
-            assert!(*weights.iter().max().unwrap() <= third);
+
+            assert!(weight <= partition_weight);
             // push backtrack frame
             let mut framecopy = frame.clone();
             framecopy.forward = false;
             stack.push(framecopy);
 
             // check win condition
-            if weights.iter().all(|w| *w == third) {
-                if frame.front_presents < least_presents || frame.entanglement < least_qe {
-                    least_presents = frame.front_presents;
-                    least_qe = frame.entanglement;
+            if weight == partition_weight {
+                if (frame.front_presents < least_presents || frame.entanglement < least_qe) {
+                    let remaining_presents: Vec<usize> = (0..presents.len()).filter(|n| !assigned[*n]).map(|n| presents[n]).collect();
+                    let mut assigned = vec![false; remaining_presents.len()];
+                    let weights = vec![0; num_groups - 1];
+                    if has_valid_combination(&remaining_presents, &mut assigned, &weights, partition_weight) {
+                        least_presents = frame.front_presents;
+                        least_qe = frame.entanglement;
+                    }
                 }
                 continue;
             }
 
-            let (i, _) = weights.iter().enumerate().find(|(i, w)| **w < third).unwrap();
-            for j in assignments.iter().enumerate().filter(|(j, a)| **a == 0).map(|(j, a)| j) {
-                let present = presents[j];
-                if weights[i] + present > third {
-                    continue;
+            assert!(weight < partition_weight);
+            let newpos = frame.pos + 1;
+            if newpos < presents.len() {
+                let present = presents[newpos];
+                stack.push(DFSFrame::new(newpos, false, frame.entanglement, frame.front_presents, true));
+                if weight + present <= partition_weight {
+                    let qe_test = frame.entanglement.checked_mul(present);
+                    if let Some(qe) = qe_test {
+                        let fp = frame.front_presents + 1;
+                        stack.push(DFSFrame::new(newpos, true, qe, fp, true));
+                    }
                 }
-                let qe = if i == 0 {frame.entanglement * present} else {frame.entanglement};
-                let fp = if i == 0 {frame.front_presents + 1} else {frame.front_presents};
-                let mut newframe = DFSFrame::new(j, i + 1, qe, fp, true);
-                stack.push(newframe);
             }
         } else {
-            let index = frame.assigned_to;
-            assert!(assignments[frame.lastassigned] == index);
-            if index == 0 {
-                continue;
+            if frame.lastassigned {
+                assert!(assigned[frame.pos] == true);
+                assigned[frame.pos] = false;
+                weight -= presents[frame.pos];
             }
-            assignments[frame.lastassigned] = 0;
-            let present = presents[frame.lastassigned];
-            weights[index - 1] -= present;
         }
     }
 
